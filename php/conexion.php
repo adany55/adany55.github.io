f9cf324cef483dcb58fd895c00cba4a1e82a55c4
danyoff0 <harcoredany@gmail.com>
2024-04-17 11:54:45 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,3 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php  
 
 
@@ -29,12 +37,9 @@ if ($stmt = $conection->prepare($query)) {
     $stmt->close();
 }
 }
-
-
-
 mysqli_close($con);
 
-
-
-
 ?>
+    
+    </body>
+</html>
